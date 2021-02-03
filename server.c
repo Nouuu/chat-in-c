309a00dfe459f7b7b7f27eb949a53880b4b6d2c2
Nouu <noelarrieulacoste@yahoo.fr>
2021-02-03 12:42:28 +0100

    rename vars

--- server.c
+++ server.c
@@ -45,12 +45,12 @@ int serverEngine(int port) {
     printf("Server on and listen on %d port\n", port);
 
 
-    while ((clientfd = accept(socketfd, (struct sockaddr *) &clientSocket, (socklen_t *) &clientSocketSize))) {
+    while ((clientFD = accept(serverFD, (struct sockaddr *) &clientSocket, (socklen_t *) &clientSocketSize))) {
         printf("Connected !\nClient IP is %s\n", inet_ntoa(clientSocket.sin_addr));
 
         int pid = fork();
         if (pid == 0) {
-            while (recv(clientfd, bufferMessage, 255, 0) > 0) {
+            while (recv(clientFD, bufferMessage, 255, 0) > 0) {
 
                 if (!strcmp(bufferMessage, "0\r\n") || !strcmp(bufferMessage, "0\n")) {
                     break;
@@ -59,13 +59,13 @@ int serverEngine(int port) {
                 memset(bufferMessage, 0, 256);
             }
             printf("%s leaving\n", inet_ntoa(clientSocket.sin_addr));
-            close(socketfd);
-            close(clientfd);
+            close(serverFD);
+            close(clientFD);
             return EXIT_SUCCESS;
         }
     }
 
-    close(socketfd);
+    close(serverFD);
     return EXIT_SUCCESS;
 }
 
