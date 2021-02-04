6a9f4b92fca50870add0df9d3c70eff58507645e
Nouu <noelarrieulacoste@yahoo.fr>
2021-02-04 16:50:47 +0100

    server command (exit and pseudo)

--- server.c
+++ server.c
@@ -9,6 +9,11 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <ctype.h>
+
+#define server_exit_command "/exit"
+#define server_pseudo_command "/pseudo"
+#define server_pseudo_max_size 10
 
 typedef struct arg_struct {
     char *ipaddress;
@@ -22,6 +27,10 @@ int initServerSocket(int port, struct sockaddr_in *serverSocket, int *socketfd);
 void serverListenLoop(int serverSocketFD);
 
 void *clientHandler(void *args);
+
+void clientListenLoop(int clientFD, const char *ip);
+
+char *getPseudo(const char *bufferMessage, const char *ip);
 
 int main(int arg, char **argv) {
     if (arg != 2) {
@@ -83,7 +92,7 @@ void serverListenLoop(int serverSocketFD) {
                                     (socklen_t *) &clientSocketSize))) {
         arg_struct arguments;
         arguments.clientSocketFD = &clientSocketFD;
-        arguments.ipaddress = inet_ntoa(clientSocket.sin_addr);
+        arguments.ipaddress = strdup(inet_ntoa(clientSocket.sin_addr));
 
         if (pthread_create(&threadId, NULL, clientHandler, (void *) &arguments) != 0) {
             perror("could not create thread\n");
@@ -94,23 +103,67 @@ void serverListenLoop(int serverSocketFD) {
 
 void *clientHandler(void *args) {
     printf("Connected\n");
-    char bufferMessage[256] = {0};
     arg_struct *arguments = (arg_struct *) args;
 
     const char *ip = arguments->ipaddress;
     int clientFD = *arguments->clientSocketFD;
 
     printf("Client connected IP is %s\n", ip);
-    while (recv(clientFD, bufferMessage, 255, 0) > 0) {
 
-        if (!strcmp(bufferMessage, "0\r\n") || !strcmp(bufferMessage, "0\n")) {
-            break;
-        }
-        printf("New message from %s : %s\n", ip, bufferMessage);
-        memset(bufferMessage, 0, 256);
-    }
+    clientListenLoop(clientFD, ip);
+
     printf("%s leaving\n", ip);
 
     close(clientFD);
     pthread_exit(NULL);
+}
+
+void clientListenLoop(int clientFD, const char *ip) {
+    char *bufferMessage = calloc(256, sizeof(char));
+    char *pseudo = NULL;
+
+    while (recv(clientFD, bufferMessage, 255, 0) > 0) {
+
+        if (strstr(bufferMessage, server_exit_command) == bufferMessage) {
+            break;
+        } else if (strstr(bufferMessage, server_pseudo_command) == bufferMessage) {
+            pseudo = getPseudo(bufferMessage, ip);
+        } else {
+            if (pseudo) {
+                printf("New message from %s (%s) : %s\n", pseudo, ip, bufferMessage);
+            } else {
+                printf("New message from %s : %s\n", ip, bufferMessage);
+            }
+        }
+        memset(bufferMessage, 0, 256);
+    }
+
+    if (pseudo != NULL) {
+        free(pseudo);
+    }
+    free(bufferMessage);
+}
+
+char *getPseudo(const char *bufferMessage, const char *ip) {
+    int command_size = strlen(server_pseudo_command);
+
+    if (!isspace(bufferMessage[command_size])) {
+        printf("Command error (%s): no space between command and arguments\n", ip);
+        return NULL;
+    }
+    int size = 0;
+    while (!isspace(bufferMessage[command_size + 1 + size])
+           && bufferMessage[command_size + 1 + size] != '\0'
+           && size <= server_pseudo_max_size) {
+        size++;
+    }
+
+    if (size > server_pseudo_max_size || size == 0) {
+        printf("Command error (%s): wrong pseudo size (between 1 and %d)\n", ip, server_pseudo_max_size);
+        return NULL;
+    }
+
+    char *pseudo = strndup(bufferMessage + command_size + 1, size);
+    printf("(%s) registered as '%s'\n", ip, pseudo);
+    return pseudo;
 }
